ad0dd30442c9b0ca024327508123d4c417ac90ba
Dragontory <toryyang03@gmail.com>
2025-01-08 23:46:44 -0500

    Added Routers and created HomePage

--- instagram-clone/src/Pages/Router/Router.jsx
+++ instagram-clone/src/Pages/Router/Router.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import HomePage from '../HomePage/HomePage';
 
 const Router = () => { 
     return (
-        <div>Router</div>
+        <div>
+            <div className = 'flex'>
+                <div>
+                    <Sidebar />
+                </div>
+                <div>
+                    <Routes>
+                        <Route path = "/" element = {<HomePage />} />
+                        {/* <Route path = '/explore' element = {<Explore />} />
+                        <Route path = '/notifications' element = {<Notifications />} />
+                        <Route path = '/messages' element = {<Messages />} />
+                        <Route path = '/profile' element = {<Profile />} /> */}
+                    </Routes>
+                </div>
+            </div>
+        </div>
     )
 }
 
