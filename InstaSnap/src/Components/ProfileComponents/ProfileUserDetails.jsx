7e6f85b719ca55d8ad11d2b3ceaf810d9cf1c31c
Dragontory <toryyang03@gmail.com>
2025-01-17 19:59:09 -0500

    Created Profile User Details

---
+++ InstaSnap/src/Components/ProfileComponents/ProfileUserDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ProfileUserDetails = () => {
+    return (
+        <div className="py-10">
+            <div>
+                <img className="w-32 h-32 rounded-full" src="https://cdn.pixabay.com/photo/2025/01/03/06/55/cortina-dampezzo-9307295_1280.jpg" alt="" />
+            </div>
+        </div>
+    )
+}
+    
