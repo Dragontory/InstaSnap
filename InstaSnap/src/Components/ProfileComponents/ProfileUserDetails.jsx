3d6c0ac9fdb186e043a90ebb0adf52cc368b610e
Dragontory <toryyang03@gmail.com>
2025-01-18 10:29:12 -0500

    Added styling to profile page

--- InstaSnap/src/Components/ProfileComponents/ProfileUserDetails.jsx
+++ InstaSnap/src/Components/ProfileComponents/ProfileUserDetails.jsx
@@ -3,14 +3,14 @@ import {TbCircleDashed} from "react-icons/tb";
 
 export const ProfileUserDetails = () => {
     return (
-        <div className="py-10">
-            <div className="flex itmes-center">
-                <div className="w-[15]">
+        <div className="py-10 w-full border">
+            <div className="flex items-center">
+                <div className="w-[25%]">
                     <img className="w-32 h-32 rounded-full" src="https://cdn.pixabay.com/photo/2025/01/03/06/55/cortina-dampezzo-9307295_1280.jpg" alt="" />
                 </div>
 
                 <div>
-                    <div>
+                    <div className="flex space-x-10 items-center">
                         <p>username</p>
                         <button>Edit Profile</button>
                         <TbCircleDashed></TbCircleDashed>
