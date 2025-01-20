24bf1b57c0745106b2ebe28cabf98674cc6d834a
Dragontory <toryyang03@gmail.com>
2025-01-19 22:49:40 -0500

    Fixed some styling on profile page

--- InstaSnap/src/Components/ProfileComponents/ProfileUserDetails.jsx
+++ InstaSnap/src/Components/ProfileComponents/ProfileUserDetails.jsx
@@ -3,9 +3,9 @@ import {TbCircleDashed} from "react-icons/tb";
 
 export const ProfileUserDetails = () => {
     return (
-        <div className="py-10 w-full border">
+        <div className="py-10 w-full">
             <div className="flex items-center">
-                <div className="w-[25%]">
+                <div className="w-[15%]">
                     <img className="w-32 h-32 rounded-full" src="https://cdn.pixabay.com/photo/2025/01/03/06/55/cortina-dampezzo-9307295_1280.jpg" alt="" />
                 </div>
 
