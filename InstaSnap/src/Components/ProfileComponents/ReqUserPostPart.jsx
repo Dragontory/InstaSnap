890d8a1055a7ef5d9af2d9b07fd3f9442fdaf60f
Dragontory <toryyang03@gmail.com>
2025-01-22 16:44:23 -0500

    Started implemented user post features

--- InstaSnap/src/Components/ProfileComponents/ReqUserPostPart.jsx
+++ InstaSnap/src/Components/ProfileComponents/ReqUserPostPart.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import { AiOutlineTable, AiOutlineUser} from 'react-icons/ai'
+import {RiVideoAddLine} from 'react-icons/ri'
+import {BiBookmark} from 'react-icons/bi'
 
 const ReqUserPostPart = () => {
+    const tabs=[
+        {
+            tab: "Posts",
+            icon: <AiOutlineTable></AiOutlineTable>,
+            activeTab: ""
+        },
+        {
+            tab: "Reels",
+            icon: <RiVideoAddLine></RiVideoAddLine>,
+        },
+        {
+            tab: "Saved",
+            icon: <BiBookmark></BiBookmark>,
+        },
+        {
+            tab: "Tagged",
+            icon: <AiOutlineUser></AiOutlineUser>,
+        },
+    ]
     return (
         <div>ReqUserPostPart</div>
     )
