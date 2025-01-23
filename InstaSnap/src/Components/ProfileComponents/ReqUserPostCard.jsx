acfa3fa3187a9b1f60e1d7e59214869a0a2df85c
Dragontory <toryyang03@gmail.com>
2025-01-23 18:03:11 -0500

    Added Heart and Comment icon to user posts

--- InstaSnap/src/Components/ProfileComponents/ReqUserPostCard.jsx
+++ InstaSnap/src/Components/ProfileComponents/ReqUserPostCard.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { AiFillHeart } from "react-icons/ai";
+import { FaComment } from "react-icons/fa";
 
 const ReqUserPostCard = () => {
     return ( 
-        <div>ReqUserPostCard</div>
+        <div>
+            <div>
+                <img className="cursor-pointer" src="https://cdn.pixabay.com/photo/2022/01/29/07/12/chinese-lion-6976588_1280.png" alt="" />
+                <div>
+                    <div>
+                        <div>
+                            <AiFillHeart></AiFillHeart> <spans>10</spans>
+                        </div>
+                        <div>
+                            <FaComment></FaComment> <spans>30</spans>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
