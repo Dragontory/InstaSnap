8f5f472bfce2f2b84336d7cb437cb51a1f727529
Dragontory <toryyang03@gmail.com>
2025-01-24 20:13:43 -0500

    implemented styling for post card

--- InstaSnap/src/Components/ProfileComponents/ReqUserPostCard.jsx
+++ InstaSnap/src/Components/ProfileComponents/ReqUserPostCard.jsx
@@ -6,10 +6,10 @@ import "./ReqUserPostCard.css";
 const ReqUserPostCard = () => {
     return ( 
         <div>
-            <div className="h-60 w-60">
+            <div className="post w-60 h-60">
                 <img className="cursor-pointer" src="https://cdn.pixabay.com/photo/2022/01/29/07/12/chinese-lion-6976588_1280.png" alt="" />
-                <div>
-                    <div>
+                <div className="overlay">
+                    <div className="overlay-text flex justify-between">
                         <div>
                             <AiFillHeart></AiFillHeart> <spans>10</spans>
                         </div>
