ede24f6bd02725e588e0a7afeed2b01f6cd4d495
Dragontory <toryyang03@gmail.com>
2025-01-25 19:32:51 -0500

    Updated home page with story icon

--- InstaSnap/src/Pages/HomePage/HomePage.jsx
+++ InstaSnap/src/Pages/HomePage/HomePage.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import StoryCircle from "../../Components/Story/StoryCircle";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <div>
+        <div>
+          <div className="storyDiv flex space-x-2 border p-4 rounded-md justify-start w-full">
+            {[1,1,1,1].map((item)=><StoryCircle />)}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
