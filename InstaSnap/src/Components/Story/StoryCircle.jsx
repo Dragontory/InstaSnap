e6ca3d4c2be546610e4aa25ce82ee6887d1bec4d
Dragontory <toryyang03@gmail.com>
2025-01-25 19:30:26 -0500

    Added styling to story icon

--- InstaSnap/src/Components/Story/StoryCircle.jsx
+++ InstaSnap/src/Components/Story/StoryCircle.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const StoryCircle = ({ story }) => {
   return (
-    <div>
+    <div className="cursor-pointer flex flex-col items-center">
       <img className="w-16 h-16 rounded-full" src="https://cdn.pixabay.com/photo/2020/11/22/11/53/sky-5766341_1280.jpg" alt="" />
+      <p>username</p>
     </div>
   )
 }
