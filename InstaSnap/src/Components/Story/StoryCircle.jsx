346e9302fd2666fdbaa04b96a0bf62fcd49c26ab
Dragontory <toryyang03@gmail.com>
2025-01-25 19:29:31 -0500

    Created and started implementing story button

---
+++ InstaSnap/src/Components/Story/StoryCircle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StoryCircle = ({ story }) => {
+  return (
+    <div>
+      <img className="w-16 h-16 rounded-full" src="https://cdn.pixabay.com/photo/2020/11/22/11/53/sky-5766341_1280.jpg" alt="" />
+    </div>
+  )
+}
+
+export default StoryCircle;
