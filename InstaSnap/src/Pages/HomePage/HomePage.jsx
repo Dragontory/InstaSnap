8cfa83a711e9686c4ba3883afd76531153dc3672
Dragontory <toryyang03@gmail.com>
2025-01-25 19:33:42 -0500

    Added styling for story icon

--- InstaSnap/src/Pages/HomePage/HomePage.jsx
+++ InstaSnap/src/Pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div>
       <div>
-        <div>
+        <div className="w-[44%] px-10">
           <div className="storyDiv flex space-x-2 border p-4 rounded-md justify-start w-full">
             {[1,1,1,1].map((item)=><StoryCircle />)}
           </div>
