bab19d7ad4d03840e84c0516c04d58aa1c090ba3
Dragontory <toryyang03@gmail.com>
2025-01-26 21:07:40 -0500

    Updated Styling for Home Page and right side

--- InstaSnap/src/Pages/HomePage/HomePage.jsx
+++ InstaSnap/src/Pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ import HomeRight from "../../Components/HomeRight/HomeRight";
 const HomePage = () => {
   return (
     <div>
-      <div>
+      <div className="mt-10 flex w-[100%] justify-center">
         <div className="w-[44%] px-10">
           <div className="storyDiv flex space-x-2 border p-4 rounded-md justify-start w-full">
             {[1,1,1,1].map((item)=><StoryCircle />)}
@@ -15,7 +15,7 @@ const HomePage = () => {
             posts
           </div>
         </div>
-        <div>
+        <div className="w-[35%]">
           <HomeRight />
         </div>
       </div>
