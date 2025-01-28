a2d8bf3e46ce34ca16d804c1c8953ac82a161058
Dragontory <toryyang03@gmail.com>
2025-01-28 18:49:01 -0500

    Added delete post option and some styling

--- InstaSnap/src/Components/Post/PostCard.jsx
+++ InstaSnap/src/Components/Post/PostCard.jsx
@@ -4,7 +4,7 @@ import {BsThreeDots} from "react-icons/bs";
 const PostCard = () => {
     return (
         <div>
-            <div>
+            <div className="border rounded-md w-full">
                 <div className="flex justify-between items-center w-full py-4 px-5">
                     <div className="flex items-center">
                         <img className="h-12 w-12 rounded-full" src="https://cdn.pixabay.com/photo/2023/11/02/14/07/minimalism-8360717_1280.jpg" alt="" />
@@ -16,6 +16,9 @@ const PostCard = () => {
                     </div>
                     <div>
                         <BsThreeDots />
+                        <div className="dropdown-content">
+                            <p className="bg-black text-white py-1 px-4 rounded-md cursor-pointer">Delete</p>
+                        </div>
                     </div>
                 </div>
             </div>
