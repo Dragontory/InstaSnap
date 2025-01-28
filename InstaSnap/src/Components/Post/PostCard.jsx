ef3664fbafd4d43964739c3a68935dcd88b24792
Dragontory <toryyang03@gmail.com>
2025-01-27 19:21:20 -0500

    Created files to develop the actual posts

---
+++ InstaSnap/src/Components/Post/PostCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomeRight = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <img src="https://cdn.pixabay.com/photo/2023/11/02/14/07/minimalism-8360717_1280.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomeRight;
