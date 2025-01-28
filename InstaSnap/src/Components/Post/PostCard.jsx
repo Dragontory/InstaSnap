36f4f63167f85d3c32db66f0455e2e03efaed7b3
Dragontory <toryyang03@gmail.com>
2025-01-27 19:28:06 -0500

    Added some styling for posts

--- InstaSnap/src/Components/Post/PostCard.jsx
+++ InstaSnap/src/Components/Post/PostCard.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import {BsThreeDots} from "react-icons/bs";
 
 const PostCard = () => {
     return (
         <div>
             <div>
-                <div>
-                    <div>
+                <div className="flex justify-between items-center w-full py-4 px-5">
+                    <div className="flex items-center">
                         <img className="h-12 w-12 rounded-full" src="https://cdn.pixabay.com/photo/2023/11/02/14/07/minimalism-8360717_1280.jpg" alt="" />
 
-                        <div>
+                        <div className="pl-2">
                             <p className="font-semibold text-sm">username</p>
                             <p className="font-thin text-sm">location</p>
                         </div>
+                    </div>
+                    <div>
+                        <BsThreeDots />
                     </div>
                 </div>
             </div>
