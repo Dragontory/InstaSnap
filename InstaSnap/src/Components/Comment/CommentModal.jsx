d7afa0e6c5ece3da560f79a0f2cf9a0e12de3f16
Dragontory <toryyang03@gmail.com>
2025-02-03 23:23:15 -0500

    Started implement comment section, still needs fixing and has bugs

--- InstaSnap/src/Components/Comment/CommentModal.jsx
+++ InstaSnap/src/Components/Comment/CommentModal.jsx
@@ -7,10 +7,19 @@ const CommentModal = ({onClose, isOpen}) => {
             <Modal onClose={onClose} isOpen={true} isCentered>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Comment</ModalHeader>
-                    <ModalCloseButton />
+                    
+
                     <ModalBody>
+                        <div>
+                            <div className="w-[45%] flex flex-col justify-center">
+                                <img className="max-h-full w-full" src="https://cdn.pixabay.com/photo/2024/10/15/17/16/swan-9122726_1280.jpg" alt="" />
+                            </div>
+                            <div className="border w-[55%]">
+
+                            </div>
+                        </div>
                     </ModalBody>
+
                 </ModalContent>
             </Modal>
         </div>
