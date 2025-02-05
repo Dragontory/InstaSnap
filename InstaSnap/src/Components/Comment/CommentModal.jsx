07097ee78b70ec19928673673f53fee2a1799c37
Dragontory <toryyang03@gmail.com>
2025-02-04 20:04:41 -0500

    Added some styling to comment modal

--- InstaSnap/src/Components/Comment/CommentModal.jsx
+++ InstaSnap/src/Components/Comment/CommentModal.jsx
@@ -4,17 +4,23 @@ import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, Modal
 const CommentModal = ({onClose, isOpen}) => {
     return (
         <div>
-            <Modal onClose={onClose} isOpen={true} isCentered>
+            <Modal size={"4xl"} onClose={onClose} isOpen={true} isCentered>
                 <ModalOverlay />
                 <ModalContent>
                     
 
                     <ModalBody>
-                        <div>
+                        <div className="h-[75vh] flex">
                             <div className="w-[45%] flex flex-col justify-center">
                                 <img className="max-h-full w-full" src="https://cdn.pixabay.com/photo/2024/10/15/17/16/swan-9122726_1280.jpg" alt="" />
                             </div>
                             <div className="border w-[55%]">
+                                <div className="flex items-center">
+                                    <div>
+                                        <img className="w-9 h-9 rounded-full" src="https://cdn.pixabay.com/photo/2023/12/16/04/29/arch-8451821_1280.jpg" alt="" />
+                                    </div>
+                                    
+                                </div>
 
                             </div>
                         </div>
