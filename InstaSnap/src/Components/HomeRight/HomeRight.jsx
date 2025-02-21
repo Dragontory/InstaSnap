0eb0901c7ab85875afa761ec5f2bb7548716db1d
Dragontory <toryyang03@gmail.com>
2025-02-20 22:39:30 -0500

    Added user info to home right side menu bar

--- InstaSnap/src/Components/HomeRight/HomeRight.jsx
+++ InstaSnap/src/Components/HomeRight/HomeRight.jsx
@@ -5,9 +5,13 @@ const HomeRight = () => {
         <div className="border">
             <div>
                 <div>
-                    <div>
+                    <div className="flex items-center">
                         <div>
                             <img className="w-12 h-12 rounded-full" src="https://cdn.pixabay.com/photo/2024/08/26/23/38/maranhao-sheets-9000410_1280.jpg" alt="" />
+                        </div>
+                        <div>
+                            <p>Full Name</p>
+                            <p>username</p>
                         </div>
                     </div>
                 </div>
