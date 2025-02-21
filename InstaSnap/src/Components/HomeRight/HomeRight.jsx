d446cb04307d8869c0670110bdcd66b2221b02dc
Dragontory <toryyang03@gmail.com>
2025-02-20 22:37:14 -0500

    Started implementing the home right side menu bar

--- InstaSnap/src/Components/HomeRight/HomeRight.jsx
+++ InstaSnap/src/Components/HomeRight/HomeRight.jsx
@@ -2,7 +2,17 @@ import React from "react";
 
 const HomeRight = () => {
     return (
-        <div className="border">HomeRight</div>
+        <div className="border">
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <img className="w-12 h-12 rounded-full" src="https://cdn.pixabay.com/photo/2024/08/26/23/38/maranhao-sheets-9000410_1280.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
