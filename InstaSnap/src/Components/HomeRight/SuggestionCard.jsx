8c287b2025e4a9db4c835724a81593a075e0c5e9
Dragontory <toryyang03@gmail.com>
2025-02-20 22:46:38 -0500

    Added follows you suggestion section and styling

--- InstaSnap/src/Components/HomeRight/SuggestionCard.jsx
+++ InstaSnap/src/Components/HomeRight/SuggestionCard.jsx
@@ -4,7 +4,11 @@ const SuggestionCard = () => {
     return (
         <div>
             <div>
-                <img src="https://cdn.pixabay.com/photo/2025/01/08/14/52/beach-9319305_1280.jpg" alt="" />
+                <img className="w-9 h-9 rounded-full" src="https://cdn.pixabay.com/photo/2025/01/08/14/52/beach-9319305_1280.jpg" alt="" />
+                <div className="ml-2">
+                    <p className="text-sm font-semibold">username</p>
+                    <p className="text-sm font-semibold opacity-70m">Follows you</p>
+                </div>
             </div>
         </div>
     )
