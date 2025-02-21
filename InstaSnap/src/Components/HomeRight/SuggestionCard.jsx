12ce5541bddbee4c2e514d6a58916766d1ebf6aa
Dragontory <toryyang03@gmail.com>
2025-02-20 22:44:39 -0500

    Started developing the suggestions component

---
+++ InstaSnap/src/Components/HomeRight/SuggestionCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SuggestionCard = () => {
+    return (
+        <div>
+            <div>
+                <img src="https://cdn.pixabay.com/photo/2025/01/08/14/52/beach-9319305_1280.jpg" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default SuggestionCard;
