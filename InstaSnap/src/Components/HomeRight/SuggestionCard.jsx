0d5079f3191fe99639323913b782ea2c9a6c0e6c
Dragontory <toryyang03@gmail.com>
2025-02-21 23:26:53 -0500

    Added styling to Suggestion parts

--- InstaSnap/src/Components/HomeRight/SuggestionCard.jsx
+++ InstaSnap/src/Components/HomeRight/SuggestionCard.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const SuggestionCard = () => {
     return (
         <div>
-            <div>
+            <div className="flex items-center">
                 <img className="w-9 h-9 rounded-full" src="https://cdn.pixabay.com/photo/2025/01/08/14/52/beach-9319305_1280.jpg" alt="" />
                 <div className="ml-2">
                     <p className="text-sm font-semibold">username</p>
                     <p className="text-sm font-semibold opacity-70m">Follows you</p>
                 </div>
             </div>
-            <p>Follow</p>
+            <p className="text-blue-700 text-sm font-semibold">Follow</p>
         </div>
     )
 }
