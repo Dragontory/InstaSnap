c7d3302cb9f53e4f4602feb3811737fb0951f1fa
Dragontory <toryyang03@gmail.com>
2025-02-23 23:43:15 -0500

    Added chakra modal template

--- InstaSnap/src/Components/Post/CreatePostModal.jsx
+++ InstaSnap/src/Components/Post/CreatePostModal.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import {Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody} from "@chakra-ui/react";
 
-const CreatePostModal = () => {
+const CreatePostModal = ({onClose, isOpen}) => {
     return (
-        <div>CreatePostModal</div>
+        <div>
+            <Modal onClick={onClose} isOpen={isOpen}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+
+
+                    </ModalBody>
+
+                </ModalContent>
+            </Modal>
+        </div>
     );
 };
