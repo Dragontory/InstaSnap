ca751eaa48bb03efab3c4966aef9ce5654be4f38
Dragontory <toryyang03@gmail.com>
2025-02-26 22:29:51 -0500

    Added create new post and share button

--- InstaSnap/src/Components/Post/CreatePostModal.jsx
+++ InstaSnap/src/Components/Post/CreatePostModal.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import {Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody} from "@chakra-ui/react";
+import {Button} from "@chakra-ui/react";
 
 const CreatePostModal = ({onClose, isOpen}) => {
     return (
         <div>
-            <Modal onClick={onClose} isOpen={true} isCentered>
+            <Modal size={"4xl"} onClick={onClose} isOpen={true} isCentered>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
+                    <div>
+                        <p>Create New Post</p>
+                        <Button className="" variant={"ghost"} size="sm" colorScheme={"blue"}>
+                            Share
+                        </Button>
+                    </div>
                     <ModalCloseButton />
                     <ModalBody>
 
