c42fbe4a572ee9395f5dee4c7e528e8aa3ccb7a5
Dragontory <toryyang03@gmail.com>
2025-04-04 20:15:43 -0400

    Created StoryViewerContainer and StoryImage elements

--- InstaSnap/src/Components/StoryComponents/StoryViewer.jsx
+++ InstaSnap/src/Components/StoryComponents/StoryViewer.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const StoryViewerContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: black;
+`
+const StoryImage = styled.img`
+    max-height: 90vh;
+    object-fit: contain;
+`
 
 const StoryViewer = () => {
     return (
-        <div>StoryViewer</div>
+        <div>
+            <StoryViewerContainer>
+                <StoryImage/>
+            </StoryViewerContainer>
+        </div>
     )
 }
 
