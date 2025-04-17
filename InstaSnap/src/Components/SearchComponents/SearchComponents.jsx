5c999e042316b2e888a5f4dfbdbe87fcf6e3412b
Dragontory <toryyang03@gmail.com>
2025-04-17 18:01:07 -0400

    Added search input and styling to search tab

--- InstaSnap/src/Components/SearchComponents/SearchComponents.jsx
+++ InstaSnap/src/Components/SearchComponents/SearchComponents.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import "./SearchComponents.css";
 
 const SearchComponents = () => {
     return (
         <div>
             <div>
                 <h1>Search</h1>
+                <input className="searchInput" type="text" placeholder="Search..." />
             </div>
         </div>
     )
