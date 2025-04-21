285795190bfa869b2b84ddfc54ebe97359356487
Dragontory <toryyang03@gmail.com>
2025-04-21 00:03:01 -0400

    Fixed issue of sidebar not showing text, but issue of clicking search and whole sidebar disappearing still exists

--- InstaSnap/src/Components/Sidebar/Sidebar.jsx
+++ InstaSnap/src/Components/Sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ import SearchComponents from "../SearchComponents/SearchComponents";
 
 const Sidebar = () => {
 
-const [activeTab, setActiveTab] = useState();
+const [activeTab, setActiveTab] = useState("");
 const navigate = useNavigate();
 const {isOpen, onClose, onOpen} = useDisclosure();
 const [isSearchVisible, setIsSearchVisible] = useState(false);
@@ -33,21 +33,21 @@ const [isSearchVisible, setIsSearchVisible] = useState(false);
         <div className="sticky top-0 h-[100vh] flex">
             <div className="flex flex-col justify-between h-full px-10">
                 <div>
-                    <div className="pt-10">
-                    <img className="w-40" src="https://i.imgur.com/zqpwkLQ.png" alt="" />
-                </div>
-                <div className="mt-10">
-                    {mainu.map((item) => (
-                        <div onClick={()=>handleTabClick(item.title)} className="flex items-center mb-5 cursor-pointer text-lg">
-                            {activeTab===item.title? item.activeIcon:item.icon}
-                            <p className={`${activeTab===item.title?"font-bold":"font-semibold"}`}>{item.title}</p>
-                        </div>
-                    ))}
-                </div>
+                    {activeTab !== "Search" && <div className="pt-10">
+                        <img className="w-40" src="https://i.imgur.com/zqpwkLQ.png" alt="" />
+                    </div>}
+                    <div className="mt-10">
+                        {mainu.map((item) => (
+                            <div onClick={()=>handleTabClick(item.title)} className="flex items-center mb-5 cursor-pointer text-lg">
+                                {activeTab===item.title? item.activeIcon:item.icon}
+                                {activeTab !== "Search" && <p className={`${activeTab===item.title?"font-bold":"font-semibold"}`}>{item.title}</p>}
+                            </div>
+                        ))}
+                    </div>
                 </div>
                 <div className="flex items-center cursor-pointer pb-10">
                     <IoReorderThreeOutline />
-                    <p className="ml-5">More</p>
+                    {activeTab !== "Search" && <p className="ml-5">More</p>}
                 </div>
             </div>
 
