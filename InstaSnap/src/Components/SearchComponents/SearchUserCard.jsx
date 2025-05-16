bbdbafb40b1aef821e97ecc5db14569f045828d5
Dragontory <toryyang03@gmail.com>
2025-05-16 13:26:32 -0400

    Added placeholder user card for search

--- InstaSnap/src/Components/SearchComponents/SearchUserCard.jsx
+++ InstaSnap/src/Components/SearchComponents/SearchUserCard.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const SearchUserCard = ({ user }) => {
     return (
-        <div>SearchUserCard</div>
+        <div>
+            <div>
+                <img className="w-10 h-10 rounded-full" src="https://cdn.pixabay.com/photo/2025/04/21/17/33/goose-9548203_1280.jpg" alt="" />
+
+                <div className="ml-3">
+                    <p>Full Name</p>
+                    <p className="opacity-70">username</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
